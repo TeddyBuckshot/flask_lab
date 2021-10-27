d61ab9aa7423dbd19e71beba9485a26329f77745
Sam <samspencer2202@outlook.com>
2021-10-27 13:56:40 +0100

    first implementation of flask

---
+++ models/order_list.py
@@ -0,0 +1,8 @@
+from models.orders import *
+
+order1 =  ("audi", 22_02_2020, 2)
+order2 = ("Ferrari", 24_06_2021, 4)
+order3 = ("Jaguar", 16_04_2019, 3)
+
+orders = [order1, order2, order3]
+
---
+++ models/orders.py
@@ -0,0 +1,6 @@
+class Orders():
+    def __init__(self, name, order_date, quantity):
+        self.name = name
+        self.order_date = order_date
+        self.quantity = quantity
+
